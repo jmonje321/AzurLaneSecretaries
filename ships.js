59b16934cb50abe48d8dfb062208391329d749d8
jmonje321 <jmonje321@gmail.com>
2022-05-10 16:43:02 -0700

    updated json

--- ships.js
+++
@@ -1,4 +0,0 @@
-let ships = '{ships: [' +
-'{"name": "Drake", "icon": "AL_Secretary/Images/DrakeIcon.png"},' +
-'{"name": "Dunkerque", "icon": "AL_Secretary/Images/DunkerqueIcon.png"},' +
-'{"name": "Monarch", "icon": "AL_Secretary/Images/MonarchIcon.png"}]}';
